d299c375575cc80721b20e0a4715b203e9d2f162
Akkil MG <akkilcharanmg@gmail.com>
2024-10-21 22:50:00 +0530

    Thank you deveesh

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -1,9 +1,9 @@
 export const siteConfig = {
     name: "Akkil M G | Portfolio",
     description:
-      "Portfolio Site of Deveesh Shetty, a Frontend Developer showcasing his projects and experiences",
+      "Portfolio Site of AkkilMG, a Backend Developer showcasing his projects and experiences",
     url: "https://akkilmg.vercel.app",
-    ogImage: "https://deveesh.vercel.app/site/og.png",
+    ogImage: "https://akkilmg.vercel.app/site/og.png",
     socials: {
       twitter: "https://twitter.com/AkkilMG",
       github: "https://github.com/AkkilMG",
